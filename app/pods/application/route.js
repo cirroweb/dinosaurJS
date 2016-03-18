d2aedcd2762d6b247a7001e3fd4c47732c12977e
fbushell <fbushell@gmail.com>
2016-03-17 18:00:34 -0600

    large fonts everywhere

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -12,3 +12,21 @@ export default Ember.Route.extend({
     }
   }
 });
+
+
+Ember.Route.reopen({
+  activate: function() {
+    var cssClass = this.toCssClass();
+    // you probably don't need the application class
+    // to be added to the body
+    if (cssClass !== 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+});
