c85f7f311fd9fc348cfc345835e8f272b4ab1f74
fbushell <fbushell@gmail.com>
2016-04-13 00:32:53 -0600

    third pass

--- app/pods/about/route.js
+++ app/pods/about/route.js
@@ -4,16 +4,18 @@ export default Ember.Route.extend({
 
   goldSponsors:  Ember.A([
     {
-      name: "Name 1",
-      img: "http://richestcelebrities.org/wp-content/uploads/2014/09/Bruce-Willis-Net-Worth.jpg",
+      name: "Turing",
+      img: "/assets/turingLogo.svg",
+      link: "https://www.turing.io/",
       description: [
         "Line 1",
         "Line 2"
       ],
     },
     {
-      name: "Mode Set",
-      img: "http://richestcelebrities.org/wp-content/uploads/2014/09/Bruce-Willis-Net-Worth.jpg",
+      name: "CA Technologies",
+      img: "/assets/CA_logo.svg",
+      link: "http://www.ca.com/us.html",
       description: [
         "Mode Set is a boutique product consultancy out of Denver, Colorado leveraging Ember and Rails. Though nerds at heart, we do more than write code. We are entrepreneurs. We are artists. We invest deeply in our clients, taking full ownership to build smart and successful products.",
       ],
