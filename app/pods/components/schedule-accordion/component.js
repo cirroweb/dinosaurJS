0faaf3036b20a652f936f6227883a682a63def85
mrbrett <brett@cirrously.com>
2016-06-24 09:27:27 -0600

    fix for iphone

--- app/pods/components/schedule-accordion/component.js
+++ app/pods/components/schedule-accordion/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
 export default Ember.Component.extend(RecognizerMixin, {
-  recognizers: 'tap',
+  recognizers: 'pan tap press',
   isOpen: false,
   actions: {
     toggleAccordion() {
