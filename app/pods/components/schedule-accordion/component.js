de044204ca2dbacacf793d0a4d57f329a6dbd34d
mrbrett <brett@cirrously.com>
2016-06-24 09:17:03 -0600

    fixes for mobile

--- app/pods/components/schedule-accordion/component.js
+++ app/pods/components/schedule-accordion/component.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
-export default Ember.Component.extend({
-  isOpen: false,
+export default Ember.Component.extend(RecognizerMixin, {
+  recognizers: 'tap',
+  isOpen: true,
   actions: {
     toggleAccordion() {
       this.toggleProperty('isOpen');
